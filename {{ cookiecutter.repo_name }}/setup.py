c15a0c511fa1130e437530fb5455dd73e15b3378
nbutterly <Nicholas.Butterly@aemo.com.au>
2023-06-14 22:54:50 +0800

    added cookiecutter field "author_email" for setup.py

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -6,5 +6,6 @@ setup(
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
+    author_email=f'{{ cookiecutter.author_email }}',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
 )
